667a473e50eed7bf1c7bc2858424393985e5880d
9walnut <kguho9202@gmail.com>
2024-01-28 12:15:41 +0900

    test

---
+++ src/test/java/com/Ohsul/Ohsul/OhsulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ohsul.Ohsul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhsulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
