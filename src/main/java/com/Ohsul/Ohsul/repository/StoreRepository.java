e536325397868607481cefc483d461819ad755df
9walnut <kguho9202@gmail.com>
2024-01-31 18:27:31 +0900

    domain new

--- src/main/java/com/Ohsul/Ohsul/repository/StoreRepository.java
+++ src/main/java/com/Ohsul/Ohsul/repository/StoreRepository.java
@@ -1,11 +1,10 @@
-package com.Ohsul.Ohsul.repository;
-
-import com.Ohsul.Ohsul.domain.Store;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-@Repository
-
-public interface  StoreRepository extends JpaRepository<Store, Long> {
-}
+//package com.Ohsul.Ohsul.repository;
+//
+//import com.Ohsul.Ohsul.domain.Store;
+//import org.springframework.stereotype.Repository;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//
+//@Repository
+//public interface  StoreRepository extends JpaRepository<Store, Long> {
+//}
