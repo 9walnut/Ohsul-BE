94b11476d68a5c7e92f83256b94880eae4361d19
9walnut <kguho9202@gmail.com>
2024-02-10 07:43:23 +0900

    ✨ Add loginController
    add loginController

--- src/main/java/com/Ohsul/Ohsul/controller/UserController.java
+++ src/main/java/com/Ohsul/Ohsul/controller/UserController.java
@@ -1,4 +1,12 @@
 package com.Ohsul.Ohsul.controller;
 
+import lombok.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/")
 public class UserController {
+
 }
